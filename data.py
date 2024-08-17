d7965bde4ddd30545a2e5a4206f04aaa0fd68c2b
INGRID YAJAIRA SANTANA GARAY <inolga17@gmail.com>
2024-08-17 09:20:21 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://029f5e89-1ed2-457a-8c61-c3ff8b9e57c2.serverhub.tripleten-services.com'
+urban_routes_url = 'https://cnt-e2766ef0-a004-4aa7-b7d6-c6cc4ceed3c8.containerhub.tripleten-services.com/'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
